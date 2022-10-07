f6e928f7d5650a295b3c0881b5e4f3704e2fb159
ktakimoto <takimoto.kengo.dev@gmail.com>
2022-10-07 20:03:14 +0900

    change: add paths for android and ghq

--- .config/zsh/rc/path.zsh
+++ .config/zsh/rc/path.zsh
@@ -13,3 +13,16 @@ export PATH="$(phpenv which phpcs):$PATH"
 # for java
 # -----
 export PATH="/usr/local/opt/openjdk@17/bin:$PATH"
+
+# for go (ghq)
+# -----
+export GOPATH=$HOME
+export PATH=$PATH:$GOPATH/bin
+
+# for android
+# -----
+# adb (Android Debug Bridge)
+export PATH=$PATH:/Users/takimoto/Library/Android/sdk/platform-tools
+
+# android-commandlinetools
+export ANDROID_SDK_ROOT="/usr/local/share/android-commandlinetools/cmdline-tools/latest/bin"
