786775ebe7d7515cf6dd0a0f5819175c8f79f00a
k.takimoto <takimoto.kengo.dev@gmail.com>
2022-10-10 20:06:09 +0900

    change: change directory structure

---
+++ src/index.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. ./src/libs/*
+
+. ./src/app/start_message.sh
+# . ./src/app/homebrew.sh
+. ./src/app/ghq.sh
+. ./src/app/link_dotfiles_to_dotpath.sh
+. ./src/app/finish_message.sh
+
+exit 0
