81ef52990fe59962fe6fd4eebdd7b1edd359cf9c
k.takimoto <55965039+takimotok@users.noreply.github.com>
2024-01-05 00:01:35 +0900

    fix<install>: fix some scripts and environments (#58)

--- src/init.sh
+++ src/init.sh
@@ -4,15 +4,33 @@ set -Ceu
 
 (
   main() {
+    if [ ! -d "$PROJECT_ROOT" ]; then
+      mkdir -p "$PROJECT_ROOT"
+    fi
     _g_cd "$PROJECT_ROOT"
 
     # check if the os is macOS
     _is_macos
     _exit_with_errors "$?"
 
+    # enable Stow to access
+    if [ ! -d "$HOME/.config" ]; then
+      mkdir -p "$HOME/.config"
+    fi
+
+    # enable 1Password-cli to access
+    if [ ! -d "$HOME/.ssh" ]; then
+      mkdir -p "$HOME/.ssh"
+    fi
+
     # set zsh
     chsh -s "$(which zsh)"
     _exit_with_errors "$?"
+
+    # copy aliases
+    if [ ! -f "$PROJECT_ROOT/.config/zsh/rc/aliases.zsh" ]; then
+      cp .config/zsh/rc/aliases.zsh.example .config/zsh/rc/aliases.zsh
+    fi
 
     # install Rosetta 2 for Apple Silicon
     if [ "$(uname -m)" = "arm64" ]; then
@@ -35,6 +53,10 @@ set -Ceu
       echo "🍺 Homebrew is already installed."
     else
       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >/dev/null
+      if [ "$(uname -m)" = "arm64" ]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+        /opt/homebrew/bin/brew tap --force homebrew/cask
+      fi
     fi
     _exit_with_errors "$?"
 
