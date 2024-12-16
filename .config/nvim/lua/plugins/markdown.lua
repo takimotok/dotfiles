431da3fdeb1f3c6492f8db5c314f847efd356307
k.takimoto <55965039+takimotok@users.noreply.github.com>
2024-12-16 14:14:08 +0900

    disable ltex LSP (#160)
    * change(nvim): disabled ltex LSP
    
    * tweak(karabiner): format by prettier
    
    * tweak(nvim): change .md Table elements

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -69,9 +69,9 @@ return {
     "dhruvasagar/vim-table-mode",
     ft = { "markdown" },
     config = function()
-      vim.g.table_mode_corner_corner = "+"
+      -- vim.g.table_mode_corner_corner = "+"
+      -- vim.g.table_mode_header_fillchar = "="
       vim.g.table_mode_delimiter = ","
-      vim.g.table_mode_header_fillchar = "="
     end,
     -- :TableModeToggle mapped to <Leader>tm by default
     -- @see: https://github.com/dhruvasagar/vim-table-mode#creating-table-on-the-fly
