45f8de872dfed06989dc8d7c8a67ff8d3c44ec04
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-01-11 06:45:27 +0900

    change: change keymap for F2

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,8 @@
 local km = require("core.key_mapper")
 
 -- add date
-km.nmap("<F2>", 'i<C-R>=strftime("%H:%M:%S")<CR>', { desc = "insert time" })
-km.imap("<F2>", '<C-R>=strftime("%H:%M:%S")<CR>', { desc = "insert time" })
+km.nmap("<F2>", 'i<C-R>=strftime("%H:%M")<CR>', { desc = "insert time" })
+km.imap("<F2>", '<C-R>=strftime("%H:%M")<CR>', { desc = "insert time" })
 km.nmap("<F3>", 'i<C-R>=strftime("%Y-%m-%d")<CR>', { desc = "insert date" })
 km.imap("<F3>", '<C-R>=strftime("%Y-%m-%d")<CR>', { desc = "insert date" })
 
