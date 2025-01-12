b5b6cbec97e1f79db6ff42788768c945d50fccd3
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-01-12 17:28:42 +0900

    change Telekasten settings (#184)
    * change: change Telekasten's defaults
    
    * change: change keymap for F2
    
    * change: change path to GDrive
    
    * change: change Telekasten settings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,10 +2,12 @@
 local km = require("core.key_mapper")
 
 -- add date
-km.nmap("<F2>", 'i<C-R>=strftime("%H:%M:%S")<CR>', { desc = "insert time" })
-km.imap("<F2>", '<C-R>=strftime("%H:%M:%S")<CR>', { desc = "insert time" })
+km.nmap("<F2>", 'i<C-R>=strftime("%H:%M")<CR>', { desc = "insert time" })
+km.imap("<F2>", '<C-R>=strftime("%H:%M")<CR>', { desc = "insert time" })
 km.nmap("<F3>", 'i<C-R>=strftime("%Y-%m-%d")<CR>', { desc = "insert date" })
 km.imap("<F3>", '<C-R>=strftime("%Y-%m-%d")<CR>', { desc = "insert date" })
+km.nmap("<F4>", 'i<C-R>=strftime("%y%m%d")<CR>', { desc = "insert date" })
+km.imap("<F4>", '<C-R>=strftime("%y%m%d")<CR>', { desc = "insert date" })
 
 -- stop the hilighting
 km.nmap("<ESC><ESC>", "<CMD>nohlsearch<CR>", { desc = "Unhighlight" })
