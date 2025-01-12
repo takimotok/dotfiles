6d7f8ef48ffb56ddac49c26047f2ba5f60140e49
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-01-12 17:27:19 +0900

    change: change Telekasten settings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,8 @@ km.nmap("<F2>", 'i<C-R>=strftime("%H:%M")<CR>', { desc = "insert time" })
 km.imap("<F2>", '<C-R>=strftime("%H:%M")<CR>', { desc = "insert time" })
 km.nmap("<F3>", 'i<C-R>=strftime("%Y-%m-%d")<CR>', { desc = "insert date" })
 km.imap("<F3>", '<C-R>=strftime("%Y-%m-%d")<CR>', { desc = "insert date" })
+km.nmap("<F4>", 'i<C-R>=strftime("%y%m%d")<CR>', { desc = "insert date" })
+km.imap("<F4>", '<C-R>=strftime("%y%m%d")<CR>', { desc = "insert date" })
 
 -- stop the hilighting
 km.nmap("<ESC><ESC>", "<CMD>nohlsearch<CR>", { desc = "Unhighlight" })
--- .config/zk/templates/default.md
+++ .config/zk/templates/default.md
@@ -1,6 +1,10 @@
 ---
+title:
 aliases: ['']
-tags: ['', '']
+tags: ['']
+id: {{id}}
+created_at: {{format-date now '%Y-%m-%d'}}
+date: {{format-date now '%Y-%m-%d'}}
 created_at: {{format-date now '%Y-%m-%d'}}
 updated_at:
 ---
