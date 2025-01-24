fad6be3e5aa6b2dfd024ea0882d4310f20e814d5
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-01-24 10:22:06 +0900

    change: make a symlink to Cursor dir.

--- src/tools.sh
+++ src/tools.sh
@@ -8,6 +8,26 @@ set -Ceu
 
     # install some languages by mise
     . ./packages/mise/install.sh
+
+    # create symlinks to other than $XDG_CONFIG_HOME
+    # -----
+    # Cursor
+    CURSOR_PATH="${HOME}/Library/Application\ Support/Cursor/User"
+    if [ ! -d "${CURSOR_PATH}" ]; then
+      mkdir -p "${CURSOR_PATH}"
+    fi
+
+    ln -sf "${PWD}/.config/cursor/settings.json" "${CURSOR_PATH}/settings.json"
+
+    # avoid error:
+    #   SecCodeCheckValidity: Error Domain=NSOSStatusErrorDomain Code=-67062 "(null)"
+    codesign --force --deep --sign - /Applications/Cursor.app
+
+    # @TODO: install extensions extensions.txt
+    # cf.) https://github.com/getcursor/cursor/issues/1985
+    #
+    # Ut seems the command below doesn't work:
+    #   `$ cursor --install-extensions`
   }
 
   main
