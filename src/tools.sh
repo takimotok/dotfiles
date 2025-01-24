b09346b88fe8626457ea858d64bf1c161172a0b6
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-01-24 10:23:34 +0900

    refine dotfiles (#202)
    * tweak: remove unused logic from tools.sh
    
    * change: make a symlink to Cursor dir.

--- src/tools.sh
+++ src/tools.sh
@@ -6,15 +6,28 @@ set -Ceu
   main() {
     _g_cd "$PROJECT_ROOT"
 
-    # copy aliases
-    if [ ! -f "$PROJECT_ROOT/.config/zsh/rc/aliases.zsh" ]; then
-      copy_from="$PROJECT_ROOT/packages/zsh/aliases.zsh.example"
-      copy_to="$PROJECT_ROOT/.config/zsh/rc/alias.zsh"
-      cp "$copy_from" "$copy_to"
-    fi
-
     # install some languages by mise
     . ./packages/mise/install.sh
+
+    # create symlinks to other than $XDG_CONFIG_HOME
+    # -----
+    # Cursor
+    CURSOR_PATH="${HOME}/Library/Application\ Support/Cursor/User"
+    if [ ! -d "${CURSOR_PATH}" ]; then
+      mkdir -p "${CURSOR_PATH}"
+    fi
+
+    ln -sf "${PWD}/.config/cursor/settings.json" "${CURSOR_PATH}/settings.json"
+
+    # avoid error:
+    #   SecCodeCheckValidity: Error Domain=NSOSStatusErrorDomain Code=-67062 "(null)"
+    codesign --force --deep --sign - /Applications/Cursor.app
+
+    # @TODO: install extensions extensions.txt
+    # cf.) https://github.com/getcursor/cursor/issues/1985
+    #
+    # Ut seems the command below doesn't work:
+    #   `$ cursor --install-extensions`
   }
 
   main
