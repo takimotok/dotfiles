518618e3eb9fe758e8f74411c899396fe6e649f4
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-01-24 09:18:15 +0900

    tweak: remove unused logic from tools.sh

--- src/tools.sh
+++ src/tools.sh
@@ -6,13 +6,6 @@ set -Ceu
   main() {
     _g_cd "$PROJECT_ROOT"
 
-    # copy aliases
-    if [ ! -f "$PROJECT_ROOT/.config/zsh/rc/aliases.zsh" ]; then
-      copy_from="$PROJECT_ROOT/packages/zsh/aliases.zsh.example"
-      copy_to="$PROJECT_ROOT/.config/zsh/rc/alias.zsh"
-      cp "$copy_from" "$copy_to"
-    fi
-
     # install some languages by mise
     . ./packages/mise/install.sh
   }
