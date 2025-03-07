7ed488ef1477f7aa044cc53757242d5505854c1f
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-03-07 16:54:14 +0900

    change: apply fidged to chat window (#235)

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -1,14 +1,5 @@
 return {
   {
     "j-hui/fidget.nvim",
-    tag = "legacy",
-    opts = {
-      text = {
-        spinner = "dots",
-        done = "✓",
-        commenced = "Started",
-        completed = "Completed",
-      },
-    },
   },
 }
