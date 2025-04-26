3a8e8c6c184071eb29e77a006145364e43668ecb
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-04-26 12:40:19 +0900

    change: skip compaudit when `$ compinit`

--- .config/zsh/rc/00-zsh-modules.zsh
+++ .config/zsh/rc/00-zsh-modules.zsh
@@ -10,27 +10,15 @@ autoload -Uz colors && colors
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export CLICOLOR=1
 
-# complement
-# -----
 # enable commands completion
-autoload -Uz compinit && compinit
-
-# 補完で小文字でも大文字にマッチさせる
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-# ../ の後は今いるディレクトリを補完しない
-zstyle ':completion:*' ignore-parents parent pwd ..
-
-# sudo の後ろでコマンド名を補完する
-zstyle ':completion:*:sudo:*' command-path \
-  /usr/local/sbin \
-  /usr/local/bin \
-  /usr/sbin \
-  /usr/bin \
-  /sbin \
-  /bin \
-  /usr/X11R6/bin
-
-# ps コマンドのプロセス名補完
-# zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
+#
+# cf.) https://gist.github.com/ctechols/ca1035271ad134841284
+# -----
+autoload -Uz compinit
+if [ "$(date +'%j')" != "$(stat -f '%Sm' -t '%j' ${ZDOTDIR}/.zcompdump 2>/dev/null)" ]; then
+    rm ${ZDOTDIR}/.zcompdump
+    compinit
+else
+    compinit -C
+fi
 
