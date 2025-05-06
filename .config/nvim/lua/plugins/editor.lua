f10ab6345d9edbbc66a04e45cf4c8d67bd765f75
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-05-06 16:23:46 +0900

    change: add im-select

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -97,4 +97,12 @@ return {
       },
     },
   },
+  {
+    "keaising/im-select.nvim",
+    event = { "InsertEnter" },
+    opts = {
+      default_im_select = "com.google.inputmethod.Japanese.Roman",
+      default_command = "macism",
+    },
+  },
 }
