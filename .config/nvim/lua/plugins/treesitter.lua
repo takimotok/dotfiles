36dfaec55fd5d1048c3f5d1295a503f4e6904b02
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-08-20 12:37:41 +0900

    fix: phactor config

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      -- ensure_installed = {},
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+    },
+  },
+}
