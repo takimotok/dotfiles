09eb4a5c18f0c820391f9064ee2c3632d3664e95
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-10-30 14:32:18 +0900

    change: setup bufferline

--- .config/nvim/lua/plugins/ui/bufferline.lua
+++ .config/nvim/lua/plugins/ui/bufferline.lua
@@ -5,6 +5,18 @@ return {
       mode = "tabs",
       show_buffer_close_icons = false,
       show_close_icon = false,
+      diagnostics = "nvim_lsp", -- false | "nvim_lsp" | "coc",
+      -- e.g.) diagnostics_indicator
+      -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      --     return "("..count..")"
+      -- end,
+      diagnostics_indicator = function(_, _, diag)
+        local icons = require("util.icons").diagnostics
+        local ret = (diag.error and icons.ERROR .. diag.error .. " " or "")
+          .. (diag.warning and icons.WARN .. diag.warning or "")
+          .. (diag.hint and icons.HINT .. diag.hint or "")
+        return vim.trim(ret)
+      end,
     },
   },
 }
