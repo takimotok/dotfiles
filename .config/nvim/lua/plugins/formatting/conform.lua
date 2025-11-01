fe8db06776cfdf6780305ac442aff44bdb9f76ab
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-11-01 18:04:04 +0900

    change: delete previous nvim-lint and conform settings

--- .config/nvim/lua/plugins/formatting/conform.lua
+++ .config/nvim/lua/plugins/formatting/conform.lua
@@ -1,6 +1,3 @@
--- local formatters_by_ft = require("plugins.formatting.conform.formatters_by_ft")
--- local formatters = require("plugins.formatting.conform.formatters")
---
 local opts = require("plugins.formatting.conform.opts")
 
 return {
--- .config/nvim/lua/plugins/linting/nvim-lint/linters/init.lua
+++
@@ -1,4 +0,0 @@
-local utils = require("plugins.linting.nvim-lint.utils")
-
----@type table<kengo.LinterName, lint.Linter|fun():lint.Linter>
-return utils.setup()
