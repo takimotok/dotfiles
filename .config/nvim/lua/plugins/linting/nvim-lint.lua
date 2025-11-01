73288f4a08eafee255bc25c8705c8dfc20b9de61
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-11-01 11:19:09 +0900

    change: add .luacheckrc

--- .config/nvim/lua/plugins/linting/nvim-lint.lua
+++ .config/nvim/lua/plugins/linting/nvim-lint.lua
@@ -7,12 +7,10 @@ return {
   config = function(_, user_opts)
     local lint = require("lint")
 
-    -- 重要: linters_by_ft を反映して、markdown では markdownlint-cli2 のみを実行
     if user_opts and user_opts.linters_by_ft then
       lint.linters_by_ft = user_opts.linters_by_ft
     end
 
-    -- FileType 決定時や保存/挿入終了時に実行
     vim.api.nvim_create_autocmd({ "FileType", "BufWritePost", "InsertLeave" }, {
       callback = function()
         lint.try_lint()
