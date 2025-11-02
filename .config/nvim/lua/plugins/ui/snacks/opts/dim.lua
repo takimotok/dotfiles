5301f5dd2ecb5f41d88c3f3cf94a2ae271f027fa
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-11-02 11:12:58 +0900

    change: set snacks-dim options

--- .config/nvim/lua/plugins/ui/snacks/opts/dim.lua
+++ .config/nvim/lua/plugins/ui/snacks/opts/dim.lua
@@ -1,3 +1,8 @@
 local M = {}
 
+-- what buffers to dim
+M.filter = function(buf)
+  return vim.g.snacks_dim ~= false and vim.b[buf].snacks_dim ~= false and vim.bo[buf].buftype == ""
+end
+
 return M
