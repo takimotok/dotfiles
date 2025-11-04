8472d132f8e7b181c8e6ccf9dc619c59d469bec5
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-11-05 07:30:19 +0900

    fix: MarkdownPreview prints Node.js v22.x.x and does nothing (#303)

--- .config/nvim/lua/plugins/coding/markdown/markdown-preview.lua
+++ .config/nvim/lua/plugins/coding/markdown/markdown-preview.lua
@@ -3,9 +3,13 @@ local md_filetypes = require("util").md_filetypes
 return {
   "iamcco/markdown-preview.nvim",
   lazy = true,
+  -- @see: ~/.local/state/nvim/lazy/readme/doc/markdown-preview.nvim.md
+  -- @see: https://github.com/iamcco/markdown-preview.nvim/issues/695
+  build = "cd app && yarn install",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = md_filetypes,
   init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
     vim.g.mkdp_theme = "light"
     vim.g.mkdp_preview_options = {
       disable_filename = 1,
