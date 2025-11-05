6f72472cd4083afc299e59f2a7cb6beaea9fe252
k.takimoto <55965039+takimotok@users.noreply.github.com>
2025-11-05 11:08:06 +0900

    fix: diable suggestion and panel

--- .config/nvim/lua/plugins/coding/ai/copilot.lua
+++ .config/nvim/lua/plugins/coding/ai/copilot.lua
@@ -1,4 +1,14 @@
 return {
   "zbirenbaum/copilot.lua",
   lazy = true,
+  event = "InsertEnter",
+  config = function()
+    -- NOTE: Avoid interfering with completions properly appearing in `blink-cmp-copilot`
+    -- to disable copilot.lua's suggestion and panel modules.
+    -- @see: https://github.com/giuxtaposition/blink-cmp-copilot
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    })
+  end,
 }
